cc0e5eac6d4e0ea01480ab86b7f64ef01dc136e7
yarncat <30006414+yaaaarn@users.noreply.github.com>
2026-06-29 07:55:29 +0200

    fix package

--- package.nix
+++ package.nix
@@ -1,17 +1,25 @@
 {
+  lib,
   buildGoModule,
-  lib
+  fetchFromGitHub,
 }:
 buildGoModule {
   pname = "yarnfetch";
-  version = "1.0.0";
-  src = ./.;
-  vendorHash = "sha256-WZTMj4x4BNAMS3PFEkvPHE1md6sLO1IWvK0yvG9drCM=";
-  
+  version = "0-unstable-2026-06-29";
+
+  src = fetchFromGitHub {
+    owner = "yaaaarn";
+    repo = "yarnfetch";
+    rev = "57ea3bd22e103a1b3f74bf16683dbfb3f7cf6ead";
+    hash = "sha256-NaksvcnnogbW9DMynrM3SshwLZFMC1DHYqJ0xG1Lf38=";
+  };
+
+  vendorHash = lib.fakeHash; # Nix will calculate the Go dependency hash
+
   meta = with lib; {
-    description = "a simple info utility";
+    description = "A simple info utility";
     homepage = "https://github.com/yaaaarn/yarnfetch";
     license = licenses.mit;
     mainProgram = "yarnfetch";
-  }; 
+  };
 }
