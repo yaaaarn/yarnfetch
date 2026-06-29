57ea3bd22e103a1b3f74bf16683dbfb3f7cf6ead
yaaaarn <30006414+yaaaarn@users.noreply.github.com>
2026-06-29 07:49:17 +0200

    flake fixes

--- package.nix
+++ package.nix
@@ -1,9 +1,17 @@
 {
   buildGoModule,
+  lib
 }:
 buildGoModule {
   pname = "yarnfetch";
   version = "1.0.0";
   src = ./.;
   vendorHash = "sha256-WZTMj4x4BNAMS3PFEkvPHE1md6sLO1IWvK0yvG9drCM=";
+  
+  meta = with lib; {
+    description = "a simple info utility";
+    homepage = "https://github.com/yaaaarn/yarnfetch";
+    license = licenses.mit;
+    mainProgram = "yarnfetch";
+  }; 
 }
